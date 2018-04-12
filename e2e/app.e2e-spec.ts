3468d5e982504a638c00f48f84d716e5645156aa
Lakshmi Soujanya <glsoujanya93@gmail.com>
2018-04-12 15:16:24 +1000

    Data binding - Quick start

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { browser, element, by } from 'protractor';
+import { Locator } from 'protractor/built/locators';
 
 describe('QuickStart E2E Tests', function () {
 
@@ -9,7 +10,7 @@ describe('QuickStart E2E Tests', function () {
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(element(<Locator>by.css('h1')).getText()).toEqual(expectedMsg);
   });
 
 });
